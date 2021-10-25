4bd91c2cc148c530a82287584b8196dc125fda5a
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-10-26 00:50:07 +0900

    test: Product 기능 구현을 위한 테스트 User

--- 52market_back/src/main/java/prgrms/al/back/user/User.java
+++ 52market_back/src/main/java/prgrms/al/back/user/User.java
@@ -1,10 +1,37 @@
 package prgrms.al.back.user;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import prgrms.al.back.product.domain.Location;
+import prgrms.al.back.product.domain.Product;
 
 @Entity(name = "user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
     @Id
+    @Column(name = "user_id")
     private Long id;
+
+    private String nickName;
+
+    @OneToMany(mappedBy = "createdBy")
+    private List<Product> products = new ArrayList<>();
+
+    @Embedded
+    private Location location;
+
+    public User(String nickName) {
+        this.nickName = nickName;
+    }
 }
