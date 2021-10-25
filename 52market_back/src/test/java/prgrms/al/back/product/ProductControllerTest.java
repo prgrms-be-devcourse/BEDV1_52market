ab716641baf3fabfb5d9b2e2b237ffefd14e3627
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-10-26 00:51:50 +0900

    test: createProduct 테스트 코드

---
+++ 52market_back/src/test/java/prgrms/al/back/product/ProductControllerTest.java
@@ -0,0 +1,75 @@
+package prgrms.al.back.product;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import prgrms.al.back.product.controller.ProductController;
+import prgrms.al.back.product.dto.ProductRequest;
+import prgrms.al.back.product.service.ProductService;
+
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+
+    MockMvc mockMvc;
+
+    @MockBean
+    ProductService productService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext,
+        RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation))
+            .build();
+    }
+
+    private String toJsonString(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    @Test
+    @DisplayName("상품을 성공적으로 등록할 경우 HTTP 상태코드 200을 반환한다.")
+    void createProduct() throws Exception {
+
+
+
+        ProductRequest productRequest = ProductRequest.builder()
+            .title("맥북 팔아요")
+            .content("싼 가격에 팔겠습니다.")
+            .price(1_000_000L)
+            .nickName("tester")
+            .build();
+
+        mockMvc.perform(post("/api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJsonString(productRequest)))
+            .andDo(print())
+            .andExpect(status().isOk());
+
+    }
+}
