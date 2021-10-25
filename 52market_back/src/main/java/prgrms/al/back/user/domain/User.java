7899422e8ec11f7bd1abfa9bb41782c852aa0f00
SangSun-Park <parkss9677@gmail.com>
2021-10-25 23:13:34 +0900

    feat: User CRUD service, repository 추가

---
+++ 52market_back/src/main/java/prgrms/al/back/user/domain/User.java
@@ -0,0 +1,54 @@
+package prgrms.al.back.user.domain;
+
+import lombok.*;
+import prgrms.al.back.attention.Attention;
+import prgrms.al.back.product.Product;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@Entity(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @Column(name = "nick_name", unique = true)
+    private String nickName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "manner_temperature")
+    private double mannerTemperature;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany
+    private List<Attention> attentionList;
+
+    @OneToMany
+    private List<Product> productList;
+
+    public void updateInfo(String name, String nickName, String password, String location) {
+        this.name = (name != null) ? name : this.name;
+        this.nickName = (nickName != null) ? nickName : this.nickName;
+        this.password = (password != null) ? password : this.password;
+        this.location = (location != null) ? location : this.location;
+    }
+}
+
+
