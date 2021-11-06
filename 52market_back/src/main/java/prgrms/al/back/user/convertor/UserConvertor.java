41bea17b4daad3847a1409417c0f2dac870cf64a
SangSunPark <parkss9677@gmail.com>
2021-11-07 05:32:00 +0900

    fix: userConvertor에 필요하지 않은 코드 삭제

--- 52market_back/src/main/java/prgrms/al/back/user/convertor/UserConvertor.java
+++ 52market_back/src/main/java/prgrms/al/back/user/convertor/UserConvertor.java
@@ -3,8 +3,6 @@ package prgrms.al.back.user.convertor;
 import org.springframework.stereotype.Component;
 import prgrms.al.back.user.domain.User;
 import prgrms.al.back.user.dto.UserDto;
-import prgrms.al.back.user.dto.UserFindResponse;
-import prgrms.al.back.user.dto.UserSignupResponse;
 
 @Component
 public class UserConvertor {
@@ -25,31 +23,6 @@ public class UserConvertor {
                 .name(userDto.getName())
                 .nickname(userDto.getNickname())
                 .password(userDto.getPassword())
-                .build();
-    }
-
-    public UserFindResponse toFindResponse(User user){
-        return UserFindResponse.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .nickname(user.getNickname())
-                .location(user.getLocation().getName())
-                .mannerTemperature(user.getMannerTemperature())
-                .createdAt(user.getCreatedAt())
-                .attentions(user.getAttentions())
-                .products(user.getProducts())
-                .sendLetters(user.getSendLetters())
-                .receiveLetters(user.getReceiveLetters())
-                .build();
-    }
-
-    public UserSignupResponse toSignResponse(User user){
-        return UserSignupResponse.builder()
-                .name(user.getName())
-                .nickname(user.getNickname())
-                .location(user.getLocation().getName())
-                .mannerTemperature(user.getMannerTemperature())
-                .createdAt(user.getCreatedAt())
                 .build();
     }
 }
